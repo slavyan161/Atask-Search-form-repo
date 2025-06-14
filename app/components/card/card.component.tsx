750f68b1170bafe304553eb5ba83c31095b51dad
rezahanggara16 <reza6546@gmail.com>
2025-06-15 02:30:54 +0700

    Add Responsive Text on Repos Card Components

--- app/components/card/card.component.tsx
+++ app/components/card/card.component.tsx
@@ -12,16 +12,16 @@ export function CardComponent({ keyId, description, score, title }: TCardCompone
             className="bg-gray-100 p-2 rounded mb-2 shadow-sm  min-h-[80px]"
         >
             <div className="flex justify-between items-center">
-                <h4 className="font-semibold">{title}</h4>
+                <h4 className="font-semibold text-sm md:text-base">{title}</h4>
                 {score !== undefined && (
-                    <span className="flex items-center gap-1">
+                    <span className="flex items-center gap-1 text-sm md:text-base pl-1 md:pl-0">
                         {score}
                         <span>⭐</span>
                     </span>
                 )}
 
             </div>
-            <p className="text-sm text-gray-600 pt-1">{description}</p>
+            <p className="text-sm text-gray-600 pt-1 text-sm md:text-base">{description}</p>
         </div>
     )
 }
